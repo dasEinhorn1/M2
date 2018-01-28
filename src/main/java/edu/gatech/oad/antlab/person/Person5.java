2506d262a02daebf6c82a2ee58228127e92ab68e
Adam Hayward <achayward1@gmail.com>
2018-01-28 10:19:21 -0500

    fixed person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Bob (and Adam Hayward)
  *  @version 1.1
  */
 public class Person5 {
@@ -25,25 +25,31 @@ public class Person5 {
 	 * 2 positions.
 	 * given "gtg123b" it should return
 	 * "g123bgt".
+	 // iterate through and add to the new string starting from the 3rd position
 	 *
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		// declare the string to return
+    String s = "";
+		for (int i = 2; i < input.length() + 2; i++) {
+			// add to the string at the index % length to loop back for the last two
+			s += input.charAt(i % input.length());
+		}
+		// return the new shifted string
+	  return s;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
 }
