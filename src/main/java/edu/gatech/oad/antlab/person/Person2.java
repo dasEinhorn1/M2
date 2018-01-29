cddad7f4500a6b67c548ab9ef926f41d3372aafd
Murtaza <Murch786@gmail.com>
2018-01-29 18:49:32 -0500

    Completed calc() method in Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,12 @@
 package edu.gatech.oad.antlab.person;
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.String;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -31,14 +34,25 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+	  ArrayList<Character> inputArrayList = new ArrayList<Character>();
+      for (char c : input.toCharArray()) {
+          inputArrayList.add(c);
+      }
+
+      String randomString = "";
+      for (int i = 0; i < input.length(); i++) {
+        Random rand = new Random();
+        int n = rand.nextInt(inputArrayList.size());
+        randomString += inputArrayList.remove(n);
+        }
+	  return randomString;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
